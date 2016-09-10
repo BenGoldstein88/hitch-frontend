aa9ec6a76fc14c5f62205222578eb6934e9e4af8
BenGoldstein88 <bengoldsteinmusic@gmail.com>
2016-09-10 16:59:42 -0700

    meaningless changes

--- app/components/Home.js
+++ app/components/Home.js
@@ -24,7 +24,7 @@ export default class Home extends React.Component {
 
   render() {
     return (
-      <div>Home sweet home!</div>
+      <h1>Home sweet home!</h1>
     );
   }
 }
--- config/routes.js
+++ config/routes.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import { Router, Route, DefaultRoute, hashHistory} from 'react-router';
-import Main from '../app/components/Main';
-import Home from '../app/components/Home';
+// import React from 'react';
+// import { Router, Route, DefaultRoute, hashHistory} from 'react-router';
+// import Main from '../app/components/Main';
+// import Home from '../app/components/Home';
 
 
-export default (
-    <Router history={hashHistory}>
-        <Route path='/' handler={Main}>
-            <DefaultRoute handler={Home} />
-        </Route>
-    </Router>
-    );
+// export default (
+//     <Router history={hashHistory}>
+//         <Route path='/' handler={Main}>
+//             <DefaultRoute handler={Home} />
+//         </Route>
+//     </Router>
+//     );
+var React = require('react');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var hashHistory = ReactRouter.hashHistory;
+var IndexRoute = ReactRouter.IndexRoute;
+var Main = require('../app/components/Main');
+var Home = require('../app/components/Home');
+
+var routes = (
+  <Router history={hashHistory}>
+    <Route path='/' handler={Main}>
+      <IndexRoute handler={Home}/>
+    </Route>
+  </Router>
+);
+
+
+
+module.exports = routes;
