4b13eb8feb56096f6b8a627f2a13dba36997d400
Olya Royall <olya.vgrig+git@gmail.com>
2016-09-10 15:54:02 -0700

    update route for config and add component tree structure

---
+++ app/components/main/PageDisplay.js
@@ -0,0 +1,1 @@
+MainPageDisplay.js
---
+++ config/routes.js
@@ -0,0 +1,19 @@
+import React, { Component, PropTypes } from 'react';
+
+class Component extends Component {
+    static propTypes = {
+        className: PropTypes.string,
+    };
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+
+        );
+    }
+}
+
+export default Component;
