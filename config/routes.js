faa431998f1427ae3e0390f732af53bc2956ab48
Olya Royall <olya.vgrig+git@gmail.com>
2016-09-10 16:59:20 -0700

    update routes to work correctly

--- config/routes.js
+++ config/routes.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Router, Route, DefaultRoute, hashHistory} from 'react-router';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import Main from '../app/components/Main';
 import Home from '../app/components/Home';
 
 
 export default (
     <Router history={hashHistory}>
-        <Route path='/' handler={Main}>
-            <DefaultRoute handler={Home} />
+        <Route path='/' component={Main}>
+            <IndexRoute component={Home} />
         </Route>
     </Router>
     );
