bbf98ee72fcd8e56d8360accb5a5c385656468ac
Olya Royall <olya.vgrig+git@gmail.com>
2016-09-10 17:09:09 -0700

    put navbar and appmain in home

---
+++ app/components/AppMain.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class AppMain extends React.Component {
+  render() {
+    return (
+      <div>AppMain</div>
+    );
+  }
+}
--- app/components/Main.js
+++ app/components/Main.js
@@ -1,34 +1,6 @@
-// import React, { Component, PropTypes } from 'react';
-
-// class Main extends Component {
-//     // static propTypes = {
-//     //     className: PropTypes.string
-//     // };
-
-//     // constructor(props) {
-//     //     super(props);
-//     // }
-
-//     render() {
-//         return (
-//             {this.props.children}
-//         );
-//     }
-// }
-
-// export default Main;
-
 import React from 'react';
 
 export default class Main extends React.Component {
-  // static propTypes = {
-  //   name: React.PropTypes.string,
-  // };
-
-  // constructor(props) {
-  //   super(props);
-  // }
-
   render() {
     return (
       <div>{this.props.children}</div>
