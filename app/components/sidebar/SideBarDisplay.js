1579bbea9da4c885d9dbd754d184f74d275f4ed3
UmarFBajwa <umarbajwa786@gmail.com>
2016-09-10 22:52:31 -0700

    npm install

--- app/components/sidebar/SideBarDisplay.js
+++ app/components/sidebar/SideBarDisplay.js
@@ -7,7 +7,12 @@ import styles from '../../styles/indexStyles'
 export default class SideBarDisplay extends React.Component {
   render() {
     return (
-      <div className={styles.sideBar.verticalAlign}>
+      <div className={styles.sideBar.verticalAlign}
+        style={{
+          backgroundImage: 'url("../../app/images/Side Bar")',
+          backgroundRepeat: 'no-repeat'
+          backgroundSize: 'contain'
+        }}>
         // <SiteMapIcon />
         // <RemindersBox />
         // <SuggestionsBox />
