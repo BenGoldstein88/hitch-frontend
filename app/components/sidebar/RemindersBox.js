5d59580a8e91b7d0bd4015a97115e58b5717a692
Olya Royall <olya.vgrig+git@gmail.com>
2016-09-10 21:18:06 -0700

    update with background images

--- app/components/sidebar/RemindersBox.js
+++ app/components/sidebar/RemindersBox.js
@@ -2,38 +2,15 @@ import React from 'react';
 import styles from '../../styles/indexStyles';
 
 export default class RemindersBox extends React.Component {
-  // constructor(props, context) {
-  //   super(props, context);
-
-  //   this.state = {
-  //     checked: false,
-  //   };
-
-  //   this.handleClick = this.handleClick.bind(this);
-
-  // };
-
-  // handleClick() {
-  //   this.setState({checked: !this.state.checked});
-  // }
   render() {
     return (
       <div style={{
-        border: '1px green solid',
+        // border: '1px green solid',
         height: '33.3%',
-        backgroundImage: 'url("../../app/images/reminders.png")'
+        width: '250px',
+        backgroundImage: 'url("../../app/images/reminders.png")',
+        backgroundSize: '100% 100%'
       }}>
-
-      // <form className={styles.suggiestionBox.horizontal}>
-      //   <label><input type="checkbox" name="task1"/>
-      //   ASK FOR A PROPOSAL</label>
-      //   <label><input type="checkbox" name="task2"/>
-      //   BOOK A FLIGHT</label>
-      //   <label><input type="checkbox" name="task3"/>
-      //   STUFF</label>
-      //   <label><input type="checkbox" name="task4"/>
-      //   ASK FOR A PROPOSAL</label>
-      // </form>
     </div>
 
     );
--- app/components/sidebar/SuggestionsBox.js
+++ app/components/sidebar/SuggestionsBox.js
@@ -5,9 +5,13 @@ export default class SuggestionsBox extends React.Component {
   render() {
     return (
       <div style={{
-      	border: '1px blue solid',
-      	height: '33.3%'
-      }}> Suggestions Box</div>
+      	// border: '1px blue solid',
+      	height: '33.3%',
+        width: '250px',
+        backgroundImage: 'url("../../app/images/suggestionbox.png")',
+        backgroundSize: '100% 100%'
+      }}>
+      </div>
 
     );
   }
