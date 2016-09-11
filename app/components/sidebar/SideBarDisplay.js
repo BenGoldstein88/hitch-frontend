4096d36351fa783c16a22789d517a7bb964a77bf
Olya Royall <olya.vgrig+git@gmail.com>
2016-09-11 00:46:00 -0700

    update viewport settings

--- app/components/sidebar/SideBarDisplay.js
+++ app/components/sidebar/SideBarDisplay.js
@@ -9,7 +9,7 @@ export default class SideBarDisplay extends React.Component {
     return (
       <div className={styles.sideBar.verticalAlign}>
         <div style={{
-          height: '100vh',
+          height: '82vh',
           marginLeft: '-15px',
           backgroundImage: 'url("../../app/images/sidebar.png")',
           backgroundRepeat: 'no-repeat',
